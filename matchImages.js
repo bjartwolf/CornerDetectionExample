f98285eeadcc7994fe94cf6e3ef9a47415a3d1e1
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2012-12-29 15:16:29 +0100

    cleaned up a bit

--- matchImages.js
+++ matchImages.js
@@ -4,23 +4,16 @@ exports.matchCornerArrays = function (corners1, corners2) {
     var sortedCorners1 = _.sortBy(corners1, function (corner) {
         return 1/corner.score;
     });
+    var leftOverCornersInCorners1 = _.last(sortedCorners1, corners1.length - corners2.length); // Is empty list if no leftovers
+    // Some sick apply-stuff here. To remove the leftOverCorners they must be single arguments to the without function
+    var sortedCornersWithoutUnmatched = _.without.apply(null, _.flatten([[sortedCorners1], leftOverCornersInCorners1], true)); 
+
+    // Reduce the list by combining to the closest value
     var memo = { sum: 0,
                  corners2: corners2,
                  unmatchedCorners: [] };
-    var leftOverCornersInCorners1 = _.last(sortedCorners1, corners1.length - corners2.length);
-    // Some sick apply-stuff here. To remove the leftOverCorners they must be single arguments to the without function
-    var sortedCornersWithoutUnmatched = _.without.apply(null, _.flatten([[sortedCorners1], leftOverCornersInCorners1], true)); 
-    console.log("sortedCorners1");
-    console.log(sortedCorners1);
-    console.log("leftOverCornersInCorners1");
-    console.log(leftOverCornersInCorners1);
-    console.log("sortedCornersWithoutUnmatched");
-    console.log(sortedCornersWithoutUnmatched);
 
-    // Reduce the list by combining to the closest value
-    console.log(sortedCornersWithoutUnmatched.length);
-    console.log(memo.corners2.length);
-    var reducedList = _.reduce(sortedCornersWithoutUnmatched, function (memo, corner) {
+     var reducedList = _.reduce(sortedCornersWithoutUnmatched, function (memo, corner) {
         var closestCorner = _.min(memo.corners2, function (corner2) {
             return (Math.pow(corner.x-corner2.x,2) + Math.pow(corner.y-corner2.y,2));
         });
@@ -28,17 +21,18 @@ exports.matchCornerArrays = function (corners1, corners2) {
         memo.corners2 = _.without(memo.corners2, closestCorner);
         return memo;
    }, memo);
-   var sum = reducedList.sum;
+
    var unMatched = reducedList.corners2;
 
-   var sumOfRest = _.reduce(unMatched, function (summemo, item) { 
-       return summemo + Math.pow(item.score,2)*1e6;
+   var sumOfRest = _.reduce(unMatched, function (sum, item) { 
+       return sum + Math.pow(item.score,2)*1e6;
    },0)
 
-   var sumOfFirst = _.reduce(leftOverCornersInCorners1, function (summemo, item) { 
-       return summemo + Math.pow(item.score,2)*1e6;
+   var sumOfFirst = _.reduce(leftOverCornersInCorners1, function (sum, item) { 
+       return sum + Math.pow(item.score,2)*1e6;
    },0)
-   return (sumOfFirst + sum + sumOfRest)/(1e8* corners1.length + 1);
+
+   return (sumOfFirst + reducedList.sum + sumOfRest)/(1e8* corners1.length + 1);
 }
 
 function getImages() {
--- test/squaredsums-test.js
+++ test/squaredsums-test.js
@@ -3,31 +3,31 @@ var fs = require('fs');
 var matchCornerArrays = require('../matchImages.js').matchCornerArrays;
 
 buster.testCase("Testing image difference metric", {
-   "//test image 1 vs 163": function () {
+   "test image 1 vs 163": function () {
         var imagedata1 = JSON.parse(fs.readFileSync('video/image1.js'));
         var imagedata2 = JSON.parse(fs.readFileSync('video/image163.js'));
         var weightedSum = matchCornerArrays(imagedata1, imagedata2);
         assert.equals(weightedSum, 100, "Hey, this should match");
      },
-     "//test image 86 vs 163": function () {
+     "test image 86 vs 163": function () {
         var imagedata1 = JSON.parse(fs.readFileSync('video/image86.js'));
         var imagedata2 = JSON.parse(fs.readFileSync('video/image163.js'));
         var weightedSum = matchCornerArrays(imagedata1, imagedata2);
         assert.equals(weightedSum, 100, "Hey, this should match");
      },
-    "//test image 164 vs 163": function () {
+    "test image 164 vs 163": function () {
         var imagedata1 = JSON.parse(fs.readFileSync('video/image164.js'));
         var imagedata2 = JSON.parse(fs.readFileSync('video/image163.js'));
         var weightedSum = matchCornerArrays(imagedata1, imagedata2);
         assert.equals(weightedSum, 100, "Hey, this should match");
      },
-    "//test image 1 vs 164": function () {
+    "test image 1 vs 164": function () {
         var imagedata1 = JSON.parse(fs.readFileSync('video/image1.js'));
         var imagedata2 = JSON.parse(fs.readFileSync('video/image164.js'));
         var weightedSum = matchCornerArrays(imagedata1, imagedata2);
         assert.equals(weightedSum, 100, "Hey, this should match");
      },
-     "//test image 86 vs 164": function () {
+     "test image 86 vs 164": function () {
         var imagedata1 = JSON.parse(fs.readFileSync('video/image86.js'));
         var imagedata2 = JSON.parse(fs.readFileSync('video/image164.js'));
         var weightedSum = matchCornerArrays(imagedata1, imagedata2);
@@ -39,12 +39,9 @@ buster.testCase("Testing image difference metric", {
         var weightedSum = matchCornerArrays(imagedata1, imagedata2);
         assert.equals(weightedSum, 100, "Hey, this should match");
      },
-     "//test image 164 vs itself": function () {
-        // Tried to be smart, but modified the same reference... 
-        // Careful
+     "test image 164 vs itself": function () {
         var imagedata1 = JSON.parse(fs.readFileSync('video/image164.js'));
-        var imagedata2 = JSON.parse(fs.readFileSync('video/image164.js'));
-        var weightedSum = matchCornerArrays(imagedata1, imagedata2);
+        var weightedSum = matchCornerArrays(imagedata1, imagedata1);
         assert.equals(weightedSum, 0, "It should match itself perfectly");
      }
 });
