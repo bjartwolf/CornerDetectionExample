23d7f9f628d3c25ee1add1c85011ad6dcebccced
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2012-12-29 16:36:25 +0100

    removed unused variable in memo

--- matchImages.js
+++ matchImages.js
@@ -4,15 +4,12 @@ exports.matchCornerArrays = function (corners1, corners2) {
     var squaredDistance = function (corner1, corner2) {
         return (Math.pow(corner1.x-corner2.x,2) + Math.pow(corner1.y-corner2.y,2));
     }
-    var sortedCorners1 = _.sortBy(corners1, function (corner) {
-        return 1/corner.score;
-    });
+    var sortedCorners1 = _.sortBy(corners1, function (corner) { return 1/corner.score; });
     var leftOverCornersInCorners1 = _.last(sortedCorners1, corners1.length - corners2.length); // Is empty list if no leftovers
     // Some sick apply-stuff here. To remove the leftOverCorners they must be single arguments to the without function
     var sortedCornersWithoutUnmatched = _.without.apply(null, _.flatten([[sortedCorners1], leftOverCornersInCorners1], true)); 
 
     // Reduce the list by combining to the closest value
-
      var reducedList = _.reduce(sortedCornersWithoutUnmatched, function (memo, corner) {
         var closestCorner = _.min(memo.corners2, function (corner2) {
             return squaredDistance(corner, corner2);
@@ -21,10 +18,8 @@ exports.matchCornerArrays = function (corners1, corners2) {
         memo.corners2 = _.without(memo.corners2, closestCorner);
         return memo;
      },
-        // Memo
         { sum: 0,
           corners2: corners2,
-          unmatchedCorners: [] 
      });
 
    var unMatched = reducedList.corners2;
@@ -36,13 +31,12 @@ exports.matchCornerArrays = function (corners1, corners2) {
    var sumOfFirst = _.reduce(leftOverCornersInCorners1, function (sum, item) { 
        return sum + item.score*1e4;
    },0)
-   return (sumOfFirst + reducedList.sum + sumOfRest)/(1e8*corners1.length + 1);
+   return (sumOfFirst + reducedList.sum + sumOfRest)/(1e6*corners1.length + 1);
 }
 
 function getImages() {
     // should contain name of image and corners
     // returns them from disk
-
     return images;
 }
 
