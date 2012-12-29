04664a778343b358c1e3331b51a8a99ea95aa173
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2012-12-29 01:04:35 +0100

    before refactroing to reduce

--- matchImages.js
+++ matchImages.js
@@ -1,34 +1,29 @@
 var _ = require('underscore');
 
 exports.matchCornerArrays = function (corners1, corners2) {
-    var sortedCorners1 = _.sortBy(corners1, function (corner) { return 1/corner.score;});
-//    var sortedCorners1 = corners1;
-    var nrOfCorners = sortedCorners1.length;
-    var sortedCorners2 = _.sortBy(corners2, function (corner) { return 1/corner.score;});
-    console.log('*****');
-    console.log(sortedCorners1);
-    console.log(sortedCorners2);
-//    var sortedCorners2 = corners2
-//    console.log("Corners1");
-//    console.log(sortedCorners1);
-//    console.log("Corners2");
-//    console.log(sortedCorners2);
-
-    var zippedArray = _.zip(sortedCorners1, sortedCorners2);
-    var squaredWeightedSum = _.reduce(zippedArray, 
-        function(memo, items) {
-            var item1 = items[0];
-            var item2 = items[1];
-            if (item1 === undefined) {
-                return memo + item2.score*(Math.pow(item2.x,2) + Math.pow(item2.y,2));
-            } else if (item2 === undefined) {
-                return memo + item1.score*(Math.pow(item1.x,2) + Math.pow(item1.y,2));
-            } else 
-            {
-                return memo + item1.score*(Math.pow((item1.x-item2.x),2) + Math.pow((item1.y-item2.y),2));
-            }
-        }, 0);
-    return squaredWeightedSum/1e6;
+    // should sort them as well by priority now
+    var sum = 0;
+    _.each(corners1, function (corner) {
+        var closestCorner = _.min(corners2, function (corner2) {
+            return (Math.pow(corner.x-corner2.x,2) + Math.pow(corner.y-corner2.y,2));
+        });
+        var indexOfClosestCorner = _.indexOf(corners2, closestCorner);
+        corners2.splice(indexOfClosestCorner,1);
+        if (closestCorner == 'Infinity') {
+            closestCorner = {};
+            closestCorner.x = -100;
+            closestCorner.y = -100;
+            corner.score = 0;
+            sum += 100000;
+        }
+        console.log(corners1);
+        sum += corner.score*(Math.pow(corner.x-closestCorner.x,2) + Math.pow(corner.y-closestCorner.y,2));
+   });
+   console.log(corners2);
+   var sumOfRest = _.reduce(corners2, function (memo, item) { 
+       return memo + Math.pow(item.score,2)*1e6;
+       },0)
+   return (sum+sumOfRest)/1e6;
 }
 
 function getImages() {
