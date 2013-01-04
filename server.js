19fb2a4551ae8b3802be18dfde3c9ed7ad1a07a4
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2013-01-04 19:31:13 +0100

    changed to using socket.io instead

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ var matchImages = require('./matchImages.js');
 var app = express(); 
 app.use(express.bodyParser());
 
-app.get('/test', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(matchImages.oneToManyMatch('image192.js'));
+var io = require('socket.io').listen(4000);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('match', function (data) {
+//        var image = { corners: data};
+        console.log(data);
+        socket.emit('result', matchImages.match(data));
+    });
 });
-app.post('/match', function (req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-    console.log(req.body);
-    res.send('ok');
-});
+
+//app.post('/match', function (req, res) {
+//    res.header("Access-Control-Allow-Origin", "*");
+//    var imageCorners = req.body.corners;
+//    if (imageCorners) {
+//        var image = { corners: imageCorners};
+//        res.send(matchImages.match(image));
+//    } else {
+//        res.send('no image');
+//    }
+//});
 app.listen(3000);
